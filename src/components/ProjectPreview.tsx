6a0a599afeae5d5ce92209f7a77898856fb77633
mattjacobson <matthew.g.jacobson@gmail.com>
2023-04-27 19:21:34 -0600

    fixing aspect ratio - scriptJSON is sometimes null

--- src/components/ProjectPreview.tsx
+++ src/components/ProjectPreview.tsx
@@ -39,7 +39,7 @@ const ProjectPreview = ({project, width=280, showDescription=false}: Props) => {
         tokenId={token?.tokenId}
         width={width}
         invocation={token?.invocation}
-        aspectRatio={parseAspectRatio(project.scriptJSON)}
+        aspectRatio={project.aspectRatio || parseAspectRatio(project.scriptJSON)}
       />
       <Box>
         <Box sx={{display: "flex", alignItems:"center"}}>
